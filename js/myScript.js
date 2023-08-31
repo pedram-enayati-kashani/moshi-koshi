d6d21617afb64701cc9ddd880544ed1b813399af
pedram enayati kashani <pedram.en1376@gmail.com>
2023-08-31 18:50:28 +0330

    add hole5 function

---
+++ js/myScript.js
@@ -0,0 +1,6 @@
+function hole5(){
+    var hole=  document.createElement("div");
+    hole.setAttribute(`id`,`hole`)
+    var parents=document.querySelector("mouse-killer-desk");
+    parents.appendChild(hole);
+}
